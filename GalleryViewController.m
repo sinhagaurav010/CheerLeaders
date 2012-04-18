65576fc8030b0ee81357a786a3e02ed20baab8a2
gaurav sinha <sinhagaurav010@gmail.com>
2012-04-19 04:03:11 +0530

    code

--- GalleryViewController.m
+++ GalleryViewController.m
@@ -39,11 +39,15 @@
         arrayImages = [[NSMutableArray  alloc] initWithObjects:@"April06-250.jpg", @"IC_janfeb07_250.jpg",@"IC_JanFeb11-250.jpg",@"IC_janfeb12-250.jpg",@"IC_julyaug06-250.jpg",@"IC_julyaug07_250.jpg",@"IC_julyaug08_250.jpg",@"IC_julycover_09-250.jpg",@"IC_june07-250.jpg",@"IC_marapr07-250.jpg",nil];
     }
     
+    
+    
     [scrllGallery setContentSize:CGSizeMake(320*[arrayImages  count], 416)];
     scrllGallery.pagingEnabled = YES;
     int incX = 0;
     for(int i=0;i<[arrayImages  count];i++)
     {
+        
+        
         UIImage *imageGal = [UIImage imageNamed:[arrayImages  objectAtIndex:i]];
         NSInteger width =  (416-imageGal.size.height)/2;
         UIImageView *imageView = [[UIImageView  alloc] initWithFrame:CGRectMake(incX, width, 320, imageGal.size.height)];
